d34a8cc6c81337e46fd22db56a6319ab131b0f87
Woen-Sug Choi <woensug.choi@gmail.com>
2025-02-04 08:41:35 +0900

    start ros2 daemon at source setup

--- setenv.sh
+++ setenv.sh
@@ -8,9 +8,11 @@ source "$HOME/VIRTUAL_ENV_ROOT/bin/activate"
 if [[ $SHELL == *"bash"* ]]; then
     # shellcheck disable=SC1091
     source "$HOME/ROS_INSTALL_ROOT/install/setup.bash"
+    ros2 daemon start
 elif [[ $SHELL == *"zsh"* ]]; then
     # shellcheck disable=SC1091
     source "$HOME/ROS_INSTALL_ROOT/install/setup.zsh"
+    ros2 daemon start
 else
     echo "Unsupported shell. Please use bash or zsh."
     exit 1
