4545cb03b7ea67ac1de5a7cfcad91e5a9f3917b2
Woen-Sug Choi <woensug.choi@gmail.com>
2025-02-04 09:11:20 +0900

    add cmake prefix path fix

--- setenv.sh
+++ setenv.sh
@@ -4,16 +4,20 @@
 # shellcheck disable=SC1091
 source "$HOME/VIRTUAL_ENV_ROOT/bin/activate"
 
+# Add CMAKE path for Qt5
+CMAKE_PREFIX_PATH=$(brew --prefix qt@5)/lib:$(brew --prefix qt@5)/lib/cmake:/opt/homebrew/opt:${CMAKE_PREFIX_PATH}
+
 # Source ROS
 if [[ $SHELL == *"bash"* ]]; then
     # shellcheck disable=SC1091
     source "$HOME/ROS_INSTALL_ROOT/install/setup.bash"
-    ros2 daemon start
 elif [[ $SHELL == *"zsh"* ]]; then
     # shellcheck disable=SC1091
     source "$HOME/ROS_INSTALL_ROOT/install/setup.zsh"
-    ros2 daemon start
 else
     echo "Unsupported shell. Please use bash or zsh."
     exit 1
 fi
+
+# Start ROS2 Daemon
+ros2 daemon start
