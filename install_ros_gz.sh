3dd17cf80b442325fb225f999523725c29872916
Woen-Sug Choi <woensug.choi@gmail.com>
2025-02-04 11:32:10 +0900

    draft

--- install_ros_gz.sh
+++ install_ros_gz.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 # ROS_GZ installation script
 
-# Read variables from the config file
+# -------- Source ROS Installation
 # shellcheck disable=SC1091
 source "$HOME/.ros2_jazzy_install_config"
-
-# Now you can use the variables in your script
-echo "Virtual Environment Root: $VIRTUAL_ENV_ROOT"
-echo "ROS Install Root: $ROS_INSTALL_ROOT"
-
-# Source ROS Installation
 # shellcheck disable=SC1090
 source "$HOME/$ROS_INSTALL_ROOT/activate_ros"
+export GZ_VERSION=harmonic
+
+# -------- Clone ROS_GZ source
+cd "$ROS_INSTALL_ROOT/src" || exit
+git clone --branch 1.0.8 https://github.com/gazebosim/ros_gz.git
+
+# -------- Build ROS_GZ
+cd "$ROS_INSTALL_ROOT" || exit
+# shellcheck disable=SC2086
+colcon build --symlink-install --packages-select ros_gz \
+    --packages-skip-by-dep python_qt_binding \
+    --cmake-args \
+    --no-warn-unused-cli \
+    -DBUILD_TESTING=OFF \
+    -DINSTALL_EXAMPLES=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
+    -DCMAKE_OSX_ARCHITECTURES=arm64 \
+    -DPython3_EXECUTABLE=$HOME/$VIRTUAL_ENV_ROOT/bin/python3 \
+    -Wno-dev --event-handlers console_cohesion+
+
+
+
+
+
+
+
 
 
 
